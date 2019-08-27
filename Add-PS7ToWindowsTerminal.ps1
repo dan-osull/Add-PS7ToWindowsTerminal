f753e8480965138785c50f18649a66d106c26da1
Dan O'Sullivan <dan@osull.com>
2019-08-27 12:23:18 -0500

    Don't rely on PS Core. Use Github for icon.

--- Add-PS7ToWindowsTerminal.ps1
+++ Add-PS7ToWindowsTerminal.ps1
@@ -4,15 +4,14 @@ $settingsFilePath = Join-Path $terminalFolderPath 'profiles.json'
 $json = Get-Content $settingsFilePath | ConvertFrom-Json
 # Get PS Core profile
 $profiles = $json.profiles
-$psCore = $profiles | Where-Object {$_.name -eq 'PowerShell Core'}
-# Make a copy of profile and configure for PS7 x64
-$ps7 = $psCore.psobject.Copy()
+# Make a copy of first profile and configure for PS7 x64
+$ps7 = $profiles[0].psobject.Copy()
 $ps7.name = 'PowerShell 7-preview (x64)'
 $ps7.commandline = 'C:\Program Files\PowerShell\7-preview\pwsh.exe'
 $ps7.guid = '{' + (New-Guid).ToString() + '}'
 # Download and set icon
 $pwsh7IconPath = Join-Path $terminalFolderPath 'pwsh7.ico'
-Invoke-WebRequest -Uri 'https://blog.osull.com/wp-content/uploads/2019/06/pwsh_32512.ico' -OutFile $pwsh7IconPath
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/weebsnore/Add-PS7ToWindowsTerminal/master/pwsh7.ico' -OutFile $pwsh7IconPath
 $ps7.icon = $pwsh7IconPath
 # Write updated settings file
 $json.profiles = $profiles + $ps7
